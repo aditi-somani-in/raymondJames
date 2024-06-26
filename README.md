ebdf53aa4af52f77c94ba998a42f7c3cd820a096
aditisomani <aditisomani5@outlook.com>
2024-06-26 17:25:05 +0530

    new project structure

--- README.md
+++
@@ -1,2 +0,0 @@
-Jira Task: https://psraymondjames.atlassian.net/browse/RJ-29
-This is the java backend code for "get" task and "complete" task.
--- src/main/java/com/puresoftware/raymondJames/utils/BearerTokenUtils.java
+++
@@ -1,8 +0,0 @@
-package com.puresoftware.raymondJames.utils;
-
-public class BearerTokenUtils {
-    public static final String GRANT_TYPE = "grant_type";
-    public static final String CLIENT_ID = "client_id";
-    public static final String CLIENT_SECRET_KEY = "client_secret";
-    public static final String BEARER="Bearer ";
-}
